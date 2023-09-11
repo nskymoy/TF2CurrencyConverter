8e166c116e09ba56a8349df9546333146d4b62c4
Nicholas Moy <nskymoy@gmail.com>
2023-09-11 19:56:36 -0400

    Changed up the font

--- components/typesofmetals.tsx
+++ components/typesofmetals.tsx
@@ -2,9 +2,9 @@ export default function TF2Metals () {
     return (
         <div>
         <img src="tf2metals.jpg" width="30%" height="auto" alt="TF2 Metals" />
-        <p>2 weapons from the same class = 1 scrap metal</p>
-        <p>3 scrap metal = 1 reclaimed metal</p>
-        <p>3 reclaimed = 1 refined metal</p>
+        <p>2 weapons from the same class = 1 scrap metal.</p>
+        <p>3 scrap metal = 1 reclaimed metal.</p>
+        <p>3 reclaimed = 1 refined metal.</p>
         </div>
     )
 }
